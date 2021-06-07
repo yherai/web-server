5698d5a8a4fd1bf7c79647b247f5260a55424fe5
Jorge Garcia <jagu.ittis@gmail.com>
2021-06-07 19:22:50 +0200

    Improve response for each route

--- src/app.js
+++ src/app.js
@@ -3,19 +3,26 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello express!');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send([
+    {
+      name: 'Jorge',
+    },
+    {
+      name: 'Charles',
+    },
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page');
+  res.send({ forecast: 'It is sunny', location: 'Madrid' });
 });
 
 app.listen(3000, () => {
