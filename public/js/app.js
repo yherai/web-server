d7ae0e75d246d56e01a886aa3b5de3eb18853a0d
Jorge Garcia Urzainqui <jagu.ittis@gmail.com>
2021-08-09 17:05:20 +0200

    Connect web-server application with web-app application

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,32 @@
 console.log('Client side js file is loaded');
+
+const weatherForm = document.querySelector('form');
+const locationInput = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  const locationValue = locationInput.value;
+
+  fetch(`http://localhost:3000/weather?address=${locationValue}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+          messageTwo.textContent = '';
+          return;
+        }
+
+        const { location, weather, temperature, feelslike } = data;
+
+        messageOne.textContent = location;
+        messageTwo.textContent = `Weather: ${weather}. Temperature: ${temperature}. Feels like: ${feelslike}`;
+      });
+    }
+  );
+});
